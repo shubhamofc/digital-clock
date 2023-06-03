092e03d0ae6c0cc86e03a71c04d9389dbabda79d
Shubham Kumar Gupta <skgupta.know@gmail.com>
2023-06-03 22:38:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+
+function clock() {
+
+const fullDate = new Date();
+const hours = fullDate.getHours();
+const mins = fullDate.getMinutes();
+const secs = fullDate.getSeconds();
+
+document.getElementById('hour').innerHTML = hours;
+document.getElementById('minute').innerHtml= mins;
+document.getElementById('second').innerHTML= secs;
+    
+}
+
+setInterval(clock,100);
+
+
